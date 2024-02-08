4faa454ef2aeb8647e915bfac1d4ea24e3166512
komerajaya456 <komerajaya7259@gmail.com>
2024-02-08 22:21:52 +0530

    nxt to be in axios/fetch to req.body

---
+++ controllers/controller.js
@@ -0,0 +1,4 @@
+const register=(req,res)=>{console.log("doned");req.body={key:"balue",};console.log(req.body);console.log(typeof(req.body))}
+
+
+module.exports=register 
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express=require("express")
 const app=express()
+const router=require('./routes/routers')
+
+app.use('/login',router)
+ 
 
 app.listen(3000,()=>{console.log(`running in port ${3000}`)})
