8a853a489a57b674b0236da1b2ed205b7d097a2b
komerajaya456 <komerajaya7259@gmail.com>
2024-02-09 19:39:10 +0530

    forms data to node using node

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Register from './components/Register'
+
+function App(){
+  return(
+    <>
+    <h1>heading</h1>
+    <Register />
+    < />
+    )
+}
+
+export default App
---
+++ client/src/components/Register.js
@@ -0,0 +1,14 @@
+import useState from 'react'
+
+
+function register(){
+  const [val,setval]
+  return(
+  <>
+    <h1>register form {500}</h1>
+    
+  </>
+    )
+}
+
+export default register;
