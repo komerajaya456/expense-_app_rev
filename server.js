a59aec19430e407e4bbe57a48e136025165c7440
komerajaya456 <komerajaya7259@gmail.com>
2024-03-12 18:07:42 +0530

    4th commit msg() in galxy

--- server.js
+++ server.js
@@ -16,17 +16,4 @@ app.listen(8080,()=>{console.log(`running in port ${8080} in chrome`)})
 //other 
 //other2
 //other 3
-
-=======
-//second 
-//third 
-//fourth
-//fifth
-//sixth1  
-<<<<<<< HEAD
-//7thr
->>>>>>> e361f0c ("")
-=======
-//7thrth
-//8 th
->>>>>>> 588d5c0 (commited 4th time in galaxy using msg (){} in bash.bashrc file)
+//other 4
